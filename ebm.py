f3bdf6d13250fe5be90b08a182b6c9ab54f5d131
marco-cartago <marco.cartago@outlook.it>
2026-06-13 12:12:01 +0200

    Increased number of channels in the first layers of the EBM network + Some formatting

--- ebm.py
+++ ebm.py
@@ -8,23 +8,28 @@ class EnergyHead(nn.Module):
 
     def __init__(self, in_dim: int, mid_dim: int) -> None:
         super(EnergyHead, self).__init__()
-        self.C1 = nn.Conv2d(1, 5, 3)
+        self.C1 = nn.Conv2d(1, 6, 3)
         self.MP1 = nn.MaxPool2d(2)
-        self.C2 = nn.Conv2d(5, 5, 3)
+        self.C2 = nn.Conv2d(6, 4, 3)
         self.MP2 = nn.MaxPool2d(2)
-        self.L3 = nn.Linear(125, 1)
+        self.C3 = nn.Conv2d(4, 4, 3)
+        self.L1 = nn.Linear(36, 16)
+        self.L2 = nn.Linear(16, 1)
         self.act = nn.GELU()
 
     def forward(self, x: torch.Tensor, single=False):
         x = self.C1(x)
         x = self.act(x)
         x = self.MP1(x)
-
         x = self.C2(x)
+        x = self.act(x)
         x = self.MP2(x)
+        x = self.C3(x)
         x = x.flatten(start_dim=1)
-
-        x = self.L3(x)
+        x = self.act(x)
+        x = self.L1(x)
+        x = self.act(x)
+        x = self.L2(x)
 
         if single:
             x = x.flatten(start_dim=0)
@@ -32,7 +37,7 @@ class EnergyHead(nn.Module):
         return x
 
 
-class EBM(nn.Module):
+class EBM(nn.Module): #InTer JoN
 
     def __init__(self, in_dim: int, mid_dim: int, n_heads: int = 4, batch_size: int = 32) -> None:
         super(EBM, self).__init__()
