65b21783176fb046e412b6cb33b3e2cb5852746d
Lorenzo <lorenzo.tonet.03@gmail.com>
2026-06-17 20:36:15 +0200

    other tests and different loss

--- refactor_code/full_loop.py
+++ refactor_code/full_loop.py
@@ -13,7 +13,7 @@ from src.train import train_one_epoch
 from src.diagnostic import diagnose
 
 
-train_loader, test_loader = load_fashion_mnist(32, shuffle=True)
+train_loader, test_loader = load_fashion_mnist(32, shuffle=True, class_subset=[9])
 
 
 model = EBM(in_dim=28*28, mid_dim=150, n_heads=3, batch_size=32)
@@ -21,7 +21,6 @@ model = EBM(in_dim=28*28, mid_dim=150, n_heads=3, batch_size=32)
 model.to(device)
 for h in model.heads:
     h.to(device)
-
 
 sampler = ReplaySampler(model, img_shape=(1, 28, 28), buffer_size=200, noise_fraction=0.05, device=device)
 
@@ -36,11 +35,12 @@ for epoch in range(15):
                                  sample_step_size=10.0,
                                  sample_noise_std=0.005,
                                  energy_reg=0.3,
-                                 corr_param=0.7,
+                                 corr_param=0.1,
                                  device=device)
     print(f"Epoch {epoch+1}, Loss: {epoch_loss:.4f}")
-    torch.save(model.state_dict(), f"checkpoint{epoch+1}_ebm_fmnist_3heads_150mid_32bsz.pth")
+    if (epoch + 1) % 5 == 0:
+        torch.save(model.state_dict(), f"checkpoint{epoch+1}_ebm_boots_3heads_150mid_32bsz.pth")
 
 diagnose(model, sampler, train_loader, test_loader, device=device, n_samples=16)
 
-torch.save(model.state_dict(), "ebm_fmnist_3heads_150mid_32bsz.pth")
+torch.save(model.state_dict(), "ebm_boots_3heads_150mid_32bsz.pth")
